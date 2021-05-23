f0c145ffb47776d55747abcb5923251ad0b7c7d0
Tim Randall <trtimrandall@gmail.com>
2021-05-23 18:31:13 +1000

    Initial Commit

---
+++ toDoSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  toDoSwift
+//
+//  Created by Tim Randall on 23/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toDoSwift/toDoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoSwiftApp.swift
+//  toDoSwift
+//
+//  Created by Tim Randall on 23/5/21.
+//
+
+import SwiftUI
+
+@main
+struct toDoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
